ef3dd55f0de682ba68b54b9bdd588817426c8c00
Bogumila Romanowska <bogurom@gmail.com>
2019-03-13 11:15:22 +0100

    Add rules to eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,25 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single",
+            {"allowTemplateLiterals": true}
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "off"
+        ]
     }
 };

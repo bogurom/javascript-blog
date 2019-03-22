21825aa7f79eb545fdf4c2d23fadaf5c0c07eeb0
Bogumila Romanowska <bogurom@gmail.com>
2019-03-22 18:52:30 +0100

    Add function calculateTagsParams

--- js/script.js
+++ js/script.js
@@ -91,6 +91,29 @@ function generateTitleLinks(customSelector = ''){
 generateTitleLinks();
 
 
+function calculateTagsParams(tags){
+
+  /* create new constant params with keys max and min */
+  const params = {
+    max: 0,
+    min: 999999
+  };
+
+  /* START LOOP for each tag in tags */
+  for(let tag in tags) {
+
+    console.log(tag + ' is used ' + tags[tag] + ' times');
+
+    /* find how often tag is used */
+    params.max = Math.max(tags[tag], params.max);
+    params.min = Math.min(tags[tag], params.min);
+
+  /* END LOOP for each tag in tags */
+  }
+return params;
+}
+
+
 function generateTags(){
   /* [NEW] create a new variable allTags with an empty object */
   let allTags = {};
@@ -152,6 +175,21 @@ function generateTags(){
   /* [NEW] add html from allTags to tagList */
   // tagList.innerHTML = allTags.join(' ');
   console.log('allTags:', allTags);
+
+  const tagsParams = calculateTagsParams(allTags);
+  console.log('tagsParams:', tagsParams);
+  /* [NEW] create variable for all links HTML code */
+  let allTagsHTML = '';
+
+  /* [NEW] START LOOP: for each tag in allTags: */
+  for(let tag in allTags){
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+    allTagsHTML += '<a href="#tag-' + tag + '">' + tag + ' (' + allTags[tag] + ') ';
+  /* [NEW] END LOOP: for each tag in allTags: */
+  }
+
+  /* [NEW] add html from allTagsHTML to tagList */
+  tagList.innerHTML = allTagsHTML;
 }
 
 generateTags();
